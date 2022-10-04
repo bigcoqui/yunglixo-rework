1563d51f5dcd1604340f903165e1f0680cdadd8c
Idklool <102881579+Its-me-a-guy@users.noreply.github.com>
2022-10-04 19:54:53 -0300

    Update MobileControlsSubState.hx

--- source/mobile/MobileControlsSubState.hx
+++ source/mobile/MobileControlsSubState.hx
@@ -35,10 +35,9 @@ class MobileControlsSubState extends FlxSubState
 
 	override function create()
 	{
-		var bg:FlxSprite = new FlxSprite().makeGraphic(FlxG.width, FlxG.height,
-			FlxColor.fromHSB(FlxG.random.int(0, 359), FlxG.random.float(0, 0.8), FlxG.random.float(0.3, 1)));
-		bg.alpha = 0.6;
-		bg.scrollFactor.set();
+		var bg:FlxSprite = new FlxSprite().loadGraphic(Paths.image('menuDesat'));
+		bg.color = 0xFFea71fd;
+		bg.screenCenter();
 		add(bg);
 
 		resetButton = new FlxButton(FlxG.width - 200, 50, 'Reset', function()
